dc914a8d78aa9c772310642b4e721d2ee1236709
imisseva <nguyenphong12a1nh3@gmail.com>
2025-11-16 12:54:58 +0700

    Modify MySQL pool configuration for SSL
    Updated database connection settings to require SSL.

--- db.js
+++ db.js
@@ -1,16 +1,21 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
-dotenv.config(); // Load biến môi trường từ .env
+dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "123456",
-  database: process.env.DB_NAME || "smartwasher",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  port: process.env.DB_PORT || 3306
+
+  // ⚠️ Aiven REQUIRE SSL
+  ssl: {
+    rejectUnauthorized: true,
+  }
 });
 
 console.log("✅ MySQL pool created (smartwasher)");
